d1ab27c7ab2734650238e2ff463b1d0cbfa03b45
Lucas Cherkewski <lucas@ecustom.ca>
2016-03-28 14:38:36 -0400

    Start to unschedule posts plugin

---
+++ vpm-unschedule-posts.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Unschedule Posts
+Description: Adds a UI to unschedule any post type.
+Version: 1.0
+Author: Van Patten Media Inc.
+Author URI: https://www.vanpattenmedia.com/
+License: GPLv2
+Text Domain: vpm
+*/
+
+class Vpm_Unschedule_Posts {
+
+}
