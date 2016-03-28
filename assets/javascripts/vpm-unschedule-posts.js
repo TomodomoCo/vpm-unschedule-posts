b792b7be04252d11b849b7cca8a24c2490268377
Lucas Cherkewski <lucas@ecustom.ca>
2016-03-28 16:31:36 -0400

    Hook into #save, not #publish, WP button

--- assets/javascripts/vpm-unschedule-posts.js
+++ assets/javascripts/vpm-unschedule-posts.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function () {
 	jQuery('#vpm-js-unschedule-post').on('click', function (event) {
 		event.preventDefault();
 
-		jQuery('#publish')
+		jQuery('#save')
 			.before('<input type="hidden" name="vpm_unschedule_post" id="vpm_unschedule_post" value="1" />')
 			.trigger('click');
 	});
